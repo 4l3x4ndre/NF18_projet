b93a21598210571116fc40168adc0d1fa31504d8
4l3x4ndre <32616333+4l3x4ndre@users.noreply.github.com>
2024-05-06 15:51:05 +0200

    Update request.sql

--- request.sql
+++ request.sql
@@ -5,6 +5,11 @@ SELECT SUM(poidsBagage) AS poids_total
 FROM Bagage B JOIN Vol V ON B.vol = V.id
 WHERE V.provenance = 'Istanbul' OR V.destination = 'Istanbul';
 
+-- Requête s'il n'y a pas de vol de telle ville
+SELECT SUM(poidsBagage) AS poids_total
+FROM Bagage B JOIN Vol V ON B.vol = V.id
+WHERE V.provenance = 'Istanbuleee' OR V.destination = 'Istanbuleee';
+
 -- Quel est le nombre de steward de chaque avion de modèle A307
 
 SELECT Avion.id, COUNT(DISTINCT STEWARTTRAVAIL.stewart) AS nb_stew
@@ -17,8 +22,8 @@ GROUP BY Avion.id;
 
 -- Quelles sont les 10 compagnies qui ont eu le plus de passagers sur les vols en partance de l'aéroport ?
 
-SELECT vol.compagnie, COUNT(bagage.passager) AS nb_pass
-FROM Vol JOIN BAGAGE ON Vol.id = BAGAGE.vol
+SELECT vol.compagnie, COUNT(DISTINCT bagage.passager) AS nb_pass
+FROM Vol LEFT OUTER JOIN BAGAGE ON Vol.id = BAGAGE.vol
 WHERE Vol.type = 'VolDepart'
 GROUP BY vol.compagnie
 ORDER BY nb_pass DESC
