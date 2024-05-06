6ad0aa6de8fd63711f63997e4141ee4e797bb095
4l3x4ndre <alexandre@amrani.fr>
2024-05-06 14:38:52 +0200

    all requests

--- request.sql
+++ request.sql
@@ -1,3 +1,26 @@
+-- Quelle est le poids des bagages venant et partant pour Istanbul ?
+-- Prendre le poids total de tous les bagages
+
 SELECT SUM(poidsBagage) AS poids_total
 FROM Bagage B JOIN Vol V ON B.vol = V.id
 WHERE V.provenance = "Istanbul" OR V.destination = "Istanbul"
+
+-- Quel est le nombre de steward de chaque avion de modèle A307
+
+SELECT vol, COUNT(*) AS nb_stew
+FROM STEWARTTRAVAIL
+JOIN Avion ON Avion.id = STEWARTTRAVAIL.vol
+JOIN Model ON Model.nom = Avion.model
+WHERE Model.nom = 'A307'
+GROUP BY vol;
+
+-- Quelles sont les 10 compagnies qui ont eu le plus de passagers sur les vols en partance de l'aéroport ?
+
+
+SELECT compagnie, COUNT(*) AS nb_pass
+FROM Vol
+WHERE type = 'VolDepart'
+GROUP BY compagnie
+ORDER BY nb_pass DESC
+LIMIT 10;
+
