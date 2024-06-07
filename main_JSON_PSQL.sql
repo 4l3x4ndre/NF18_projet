2b6301b322834e1b0814d58ec62e96113d9a2d60
4l3x4ndre <alexandre@amrani.fr>
2024-06-07 10:58:08 +0200

    schema & sql non-relationnel

--- main_JSON_PSQL.sql
+++ main_JSON_PSQL.sql
@@ -1,8 +1,27 @@
-DROP TABLE IF EXISTS VolNR;
-DROP TYPE IF EXISTS TypeVolNR;
+CREATE SCHEMA IF NOT EXISTS schema_json_psql;
+SET search_path = schema_json_psql;
 
-CREATE TYPE TypeVolNR AS ENUM ('VolArrivee', 'VolDepart');
-CREATE TABLE VolNR (
+DROP TABLE IF EXISTS schema_json_psql.VolNR;
+DROP TYPE IF EXISTS schema_json_psql.TypeVolNR;
+DROP TABLE IF EXISTS schema_json_psql.Porte;
+DROP TABLE IF EXISTS schema_json_psql.Terminal;
+
+
+CREATE TABLE schema_json_psql.Terminal (
+    nom VARCHAR(50) PRIMARY KEY,
+    longitude FLOAT CHECK (longitude BETWEEN -180 AND 180),
+    latitude FLOAT CHECK (latitude BETWEEN -90 AND 90),
+    CONSTRAINT unique_long_lat UNIQUE (longitude, latitude)
+);
+
+CREATE TABLE schema_json_psql.Porte (
+    terminal VARCHAR(50) REFERENCES schema_json_psql.Terminal(nom),
+    num INT CHECK (num > 0),
+    PRIMARY KEY (terminal, num)
+);
+
+CREATE TYPE schema_json_psql.TypeVolNR AS ENUM ('VolArrivee', 'VolDepart');
+CREATE TABLE schema_json_psql.VolNR (
     	id VARCHAR(10),
 	compagnieVol VARCHAR(50),
 	porte_num INT NOT NULL,
@@ -12,29 +31,57 @@ CREATE TABLE VolNR (
 	heureEmbarquement TIMESTAMP,
 	provenance VARCHAR(100),
 	heureArrivee TIMESTAMP,
-	type TypeVolNR NOT NULL,
+	type schema_json_psql.TypeVolNR NOT NULL,
 	bagage JSON NOT NULL,
 	passager JSON,
 	avion JSON NOT NULL,
-	PRIMARY KEY(id),
-	CHECK ( ((destination IS NOT NULL AND heureDepart IS NOT NULL AND heureEmbarquement IS NOT NULL AND heureEmbarquement < heureDepart AND type='VolDepart' ) AND (heureArrivee IS NULL AND provenance IS NULL)) OR ((heureArrivee IS NOT NULL AND provenance IS NOT NULL) AND (destination IS NULL AND heureDepart IS NULL AND heureEmbarquement IS NULL AND type='VolArrivee')) ) 
+	pilote1 JSON NOT NULL,
+	pilote2 JSON NOT NULL,
+	FOREIGN KEY (porte_term,porte_num) REFERENCES schema_json_psql.Porte(terminal,num),
+	CHECK ( ((destination IS NOT NULL AND heureDepart IS NOT NULL AND heureEmbarquement IS NOT NULL AND heureEmbarquement < heureDepart AND type='VolDepart' ) AND (heureArrivee IS NULL AND provenance IS NULL)) OR ((heureArrivee IS NOT NULL AND provenance IS NOT NULL) AND (destination IS NULL AND heureDepart IS NULL AND heureEmbarquement IS NULL AND type='VolArrivee')) ),
+	PRIMARY KEY(id)
 );
 
+-- Insertion dans la table Terminal
+INSERT INTO schema_json_psql.Terminal (nom, longitude, latitude)
+VALUES
+    ('Terminal 1', 48.8566, 2.3522),
+    ('Terminal 2', 45.7640, 4.8357),
+    ('Terminal 3', 43.2965, 5.3698),
+    ('Terminal 4', 40.4168, -3.7038);
+
+-- Insertion dans la table Porte
+INSERT INTO schema_json_psql.Porte (terminal, num)
+VALUES
+    ('Terminal 1', 1),
+    ('Terminal 1', 2),
+    ('Terminal 1', 3),
+    ('Terminal 2', 1),
+    ('Terminal 2', 2),
+    ('Terminal 3', 1),
+    ('Terminal 4', 1),
+    ('Terminal 4', 2);
+
 -- Exemple d'insertion dans la table flights
-INSERT INTO VolNR VALUES
+INSERT INTO schema_json_psql.VolNR VALUES
 	
 ('VOL001', 'Air France', 1, 'Terminal 1',  'New York', '2024-06-10 08:00:00', '2024-06-10 07:00:00', NULL, NULL, 'VolDepart', 
 	'[{"passager_id":1, "nombreBagage":2, "poidsBagage":30.5}, {"passager_id":2, "nombreBagage":2, "poidsBagage":40.5}, {"passager_id":3, "nombreBagage":4, "poidsBagage":35}, {"passager_id":4, "nombreBagage":0, "poidsBagage":0}]', 
-	'[{"passager_id":1, "nom":"Dupont", "prenom":"Jean", "dateNaiss":"1990-05-15", "rue":"123 Rue de la liberté", "codepostal":75001, "ville":"Paris", "pays":"France", "numeroTel":"0683058729"}, {"passager_id":2, "nom":"Andre", "prenom":"Jeanne", "dateNaiss":"1980-05-15", "rue":"124 Rue de la prison", "codepostal":75001, "ville":"Paris", "pays":"France", "numeroTel":"0692658294"}, {"passager_id":3, "nom":"Dupuit", "prenom":"Jean-Charles", "dateNaiss":"1950-05-15", "rue":"125 Rue de la paix", "codepostal":75002, "ville":"Paris", "pays":"France", "numeroTel":"0690572927"}, {"passager_id":4, "nom":"Dondon", "prenom":"Jean-Pierre", "dateNaiss":"1960-05-15", "rue":"126 Rue de la guerre", "codepostal":60002, "ville":"Compiègne", "pays":"France", "numeroTel":"0726492749"}]', 
-	'{"avion_id":"ABC123","model":"A307", "compagnieFabrication":"Airbus", "nombrePlaces":400, "vitesseMax":1200.0, "type":"TurboReacteur", "pousseeMax":850.0}'),
+	'[{"id_personne":1, "nom":"Dupont", "prenom":"Jean", "dateNaiss":"1990-05-15", "rue":"123 Rue de la liberté", "codepostal":"75001", "ville":"Paris", "pays":"France", "numeroTel":"0683058729"}, {"id_personne":2, "nom":"Andre", "prenom":"Jeanne", "dateNaiss":"1980-05-15", "rue":"124 Rue de la prison", "codepostal":"75001", "ville":"Paris", "pays":"France", "numeroTel":"0692658294"}, {"id_personne":3, "nom":"Dupuit", "prenom":"Jean-Charles", "dateNaiss":"1950-05-15", "rue":"125 Rue de la paix", "codepostal":"75002", "ville":"Paris", "pays":"France", "numeroTel":"0690572927"}, {"id_personne":4, "nom":"Dondon", "prenom":"Jean-Pierre", "dateNaiss":"1960-05-15", "rue":"126 Rue de la guerre", "codepostal":"60002", "ville":"Compiègne", "pays":"France", "numeroTel":"0726492749"}]', 
+	'{"avion_id":"ABC123","model":"A307", "compagnieFabrication":"Airbus", "nombrePlaces":400, "vitesseMax":1200.0, "type":"TurboReacteur", "pousseeMax":850.0}', 
+	'{"id_personne":6, "numeroTel":"0657283758", "nom":"Sully", "prenom":"Jake", "dateNaiss":"1970-05-20", "rue":"12 St. of Peace", "codepostal":"10100", "ville":"New-York", "pays":"Etats-Unis"}',
+	'{"id_personne":7, "numeroTel":"0746289274", "nom":"Olang", "prenom":"Paul", "dateNaiss":"1975-12-20", "rue":"11B St. Baker Street", "codepostal":"12001", "ville":"Londres", "pays":"Angleterre"}'),
 
 ('VOL002', 'Air France', 3, 'Terminal 1',  'Paris', '2024-05-10 08:00:00', '2024-05-10 07:00:00', NULL, NULL, 'VolDepart', 
 	'[{"passager_id":1, "nombreBagage":2, "poidsBagage":30.5}, {"passager_id":2, "nombreBagage":4, "poidsBagage":60.5}]', 
-	'[{"passager_id":1, "nom":"Dupont", "prenom":"Jean", "dateNaiss":"1990-05-15", "rue":"123 Rue de la liberté", "codepostal":75001, "ville":"Paris", "pays":"France", "numeroTel":"0683058729"}, {"passager_id":2, "nom":"Andre", "prenom":"Jeanne", "dateNaiss":"1980-05-15", "rue":"124 Rue de la prison", "codepostal":75001, "ville":"Paris", "pays":"France", "numeroTel":"0692658294"}]', 
-	'{"avion_id":"DEF456","model":"B737", "compagnieFabrication":"Boeing", "nombrePlaces":450, "vitesseMax":1200.0, "type":"TurboPropulseur", "vitMaxRot":1040.0}'),
+	'[{"id_personne":1, "nom":"Dupont", "prenom":"Jean", "dateNaiss":"1990-05-15", "rue":"123 Rue de la liberté", "codepostal":"75001", "ville":"Paris", "pays":"France", "numeroTel":"0683058729"}, {"id_personne":2, "nom":"Andre", "prenom":"Jeanne", "dateNaiss":"1980-05-15", "rue":"124 Rue de la prison", "codepostal":"75001", "ville":"Paris", "pays":"France", "numeroTel":"0692658294"}]', 
+	'{"avion_id":"DEF456","model":"B737", "compagnieFabrication":"Boeing", "nombrePlaces":450, "vitesseMax":1200.0, "type":"TurboPropulseur", "vitMaxRot":1040.0}',
+	'{"id_personne":8, "numeroTel":"0746289274", "nom":"Schneider", "prenom":"Anna", "dateNaiss":"1978-02-20", "rue":"101 Rue du Commerce", "codepostal":"75002", "ville":"Paris", "pays":"France"}',
+	'{"id_personne":7, "numeroTel":"0746289274", "nom":"Olang", "prenom":"Paul", "dateNaiss":"1975-12-20", "rue":"11B St. Baker Street", "codepostal":"12001", "ville":"Londres", "pays":"Angleterre"}'),
 
 ('VOL003', 'British Airways', 2, 'Terminal 2',  NULL, NULL, NULL, 'Istanbul', '2084-05-10 08:00:00', 'VolArrivee', 
 	'[{"passager_id":3, "nombreBagage":4, "poidsBagage":35}, {"passager_id":4, "nombreBagage":0, "poidsBagage":0}]', 
-	'[{"passager_id":3, "nom":"Dupuit", "prenom":"Jean-Charles", "dateNaiss":"1950-05-15", "rue":"125 Rue de la paix", "codepostal":75002, "ville":"Paris", "pays":"France", "numeroTel":"0690572927"}, {"passager_id":4, "nom":"Dondon", "prenom":"Jean-Pierre", "dateNaiss":"1960-05-15", "rue":"126 Rue de la guerre", "codepostal":60002, "ville":"Compiègne", "pays":"France", "numeroTel":"0726492749"}]', 
-	'{"avion_id":"GHI789","model":"A307", "compagnieFabrication":"Airbus", "nombrePlaces":400, "vitesseMax":1200.0, "type":"TurboReacteur", "pousseeMax":850.0}');
-
+	'[{"id_personne":3, "nom":"Dupuit", "prenom":"Jean-Charles", "dateNaiss":"1950-05-15", "rue":"125 Rue de la paix", "codepostal":"75002", "ville":"Paris", "pays":"France", "numeroTel":"0690572927"}, {"id_personne":4, "nom":"Dondon", "prenom":"Jean-Pierre", "dateNaiss":"1960-05-15", "rue":"126 Rue de la guerre", "codepostal":"60002", "ville":"Compiègne", "pays":"France", "numeroTel":"0726492749"}]', 
+	'{"avion_id":"GHI789","model":"A307", "compagnieFabrication":"Airbus", "nombrePlaces":400, "vitesseMax":1200.0, "type":"TurboReacteur", "pousseeMax":850.0}',
+	'{"id_personne":6, "numeroTel":"0657283758", "nom":"Sully", "prenom":"Jake", "dateNaiss":"1970-05-20", "rue":"12 St. of Peace", "codepostal":"10100", "ville":"New-York", "pays":"Etats-Unis"}',
+	'{"id_personne":7, "numeroTel":"0746289274", "nom":"Olang", "prenom":"Paul", "dateNaiss":"1975-12-20", "rue":"11B St. Baker Street", "codepostal":"12001", "ville":"Londres", "pays":"Angleterre"}');
---
+++ schema_JSON_PSQL.md
@@ -0,0 +1,69 @@
+| Information                   | Valeur                               |
+| --                            | --                                   |
+| Version du document           | 1.0.0                                |
+| Dernière modification         | 2024-06-07                           |
+| Auteurs | AMRANI Alexandre, DELBECQUE Lucas, DELMAERE Mathis, LAMY Léo |
+
+---
+
+**<mark style='background:#ffadad'>Tous les champs sont NOT NULL par défaut.</mark>** 
+
+### Attribut JSON bagage (Type : JSON)
+
+Données attendues : Une liste d'objets représentant les bagages des passagers. Chaque objet doit contenir :
+- passager_id (Type : Entier) : Identifiant unique du passager (référencent `id_personne` du champ JSON passager).
+- nombreBagage (Type : Entier) : Nombre de bagages enregistrés par le passager.
+- poidsBagage (Type : Flottant) : Poids total des bagages du passager en kilogrammes.
+
+### Attribut JSON passager (Type : JSON)
+
+Données attendues : Une liste d'objets représentant les passagers du vol. Chaque objet doit contenir :
+- id_personne (Type : Entier) : Identifiant unique de la personne.
+- nom (Type : Chaîne de caractères) : Nom de famille du passager.
+- prenom (Type : Chaîne de caractères) : Prénom du passager.
+- dateNaiss (Type : Date) : Date de naissance du passager au format AAAA-MM-JJ.
+- rue (Type : Chaîne de caractères) : numéro et rue de l'adresse du passager.
+- codepostal (Type : Chaîne de caractères) : Code postal de l'adresse du passager.
+	- Le type est chaîne de caractères car en Javascript (donc JSON) le préfixe 0 défini le type octal
+	- Il est donc attendu une chaîne représentant le code postal en entier, c'est-à-dire comprenant tous les zéros. Ce champ représente un entier positif.
+- ville (Type : Chaîne de caractères) : Ville de résidence du passager.
+- pays (Type : Chaîne de caractères) : Pays de résidence du passager.
+- numeroTel (Type : Chaîne de caractères) : Numéro de téléphone du passager.
+	- Le format est 10 chiffres ou 9 chiffres précédés d'un indicatif téléphonique ("+33")
+
+### Attribut JSON avion (Type : JSON)
+
+Données attendues : Un objet représentant les informations sur l'avion. L'objet doit contenir :
+- avion_id (Type : Chaîne de caractères) : Identifiant unique de l'avion.
+- model (Type : Chaîne de caractères) : Modèle de l'avion.
+- compagnieFabrication (Type : Chaîne de caractères) : Compagnie ayant fabriqué l'avion.
+- nombrePlaces (Type : Entier) : Nombre de places disponibles dans l'avion.
+	- Doit être supérieur ou égale à zéro.
+- vitesseMax (Type : Flottant) : Vitesse maximale de l'avion en km/h.
+	- Doit être supérieur à zéro.
+- type (Type : Chaîne de caractères) : Type de moteur de l'avion 
+	- choix obligatoire parmi : TurboRéacteur; TurboPropulseur.
+- pousseeMax (Type : Flottant, optionnel) : Poussée maximale des moteurs de l'avion (en kN).
+	- Doit être supérieur à zéro.
+
+Précisions :
+- Si le type est TurboRéacteur, alors la vitesseMax est renseignée et non pousseeMax
+- Si le type est TurboPropulseur, alors la pousseeMax est renseignée et non vitesseMax
+
+### Attribut JSON pilote1 et pilote2 (Type : JSON)
+
+Données attendues : Un objet représentant les informations sur le premier pilote et le deuxième pilote. L'objet doit contenir :
+- id_personne (Type : Entier) : Identifiant unique du pilote.
+- numeroTel (Type : Chaîne de caractères) : Numéro de téléphone du pilote.
+	- Le format est 10 chiffres ou 9 chiffres précédés d'un indicatif téléphonique ("+33")
+- nom (Type : Chaîne de caractères) : Nom de famille du pilote.
+- prenom (Type : Chaîne de caractères) : Prénom du pilote.
+- dateNaiss (Type : Date) : Date de naissance du pilote au format AAAA-MM-JJ.
+- rue (Type : Chaîne de caractères) : numéro et rue de l'adresse du pilote.
+- codepostal (Type : Chaîne de caractères) : Code postal de l'adresse du pilote.
+	- Le type est chaîne de caractères car en Javascript (donc JSON) le préfixe 0 défini le type octal
+- ville (Type : Chaîne de caractères) : Ville de résidence du pilote.
+- pays (Type : Chaîne de caractères) : Pays de résidence du pilote.
+
+Précisions : 
+- Les champs pilote1 et pilote2 doivent être différents : ils indiquent les deux pilotes, différents, du vol.
